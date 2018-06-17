5df89745d865a045e2e633262e93fbf8e9da0a44
Loic <cryptodraco@gmail.com>
2018-06-17 18:05:59 +0200

    refacto (1)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,43 +2,23 @@
   <div class="hello">
     <div class="columns isfullheigth">
       <div class="column is-one-fifth has-background-primary isfullheigth">
-        <div id="uuu">elmt</div>
         <div>elmt</div>
-        <div>elmt</div>
-        <drag class="drag" :transfer-data="{ example: 'styling' }">drag</drag>
+        <drag class="drag" :transfer-data="{type: 'map_scenario'}">drag</drag>
       </div>
       <div class="column has-background-white-ter">
 
         <ul id="example-1">
           <li v-for="(item, index) in items">
 
-            <div v-if="item.type == 'map_scenario'" class="map-scenario isfullheigth">
-              <vue-slider v-bind="item.value" class="slide-epoc-range"></vue-slider>
-              <div class="columns isfullheigth">
-                <div class="column has-background-primary is-one-fifth">
-                  <div>elmt</div>
-                  <a class="button is-primary is-inverted is-outlined">carte</a>
-                </div>
+            <div v-if="item.type == 'map_scenario'">
+              <chronoScene propitem=item></chronoScene>
+            </div> <!-- if -->
 
-                <div class="column isfullheigth relative">
-
-                  <div id="zanime" class="asset" style="position: absolute;" v-for="(asset, index) in assets" v-draggable="draggableValue">
-                      {{asset.layerX}} {{asset.layerY}}
-                  </div>
-                  <!-- -->
-                  <div class="map-preview" v-if="imageData.length > 0">
-                    <img :src="imageData">
-                  </div>
-                  <drop class="drop map-insert" @drop="handleFileDrop">insert map</drop>
-                </div>
-              </div>
-            </div>
-
-            <drop v-else class="drop flow-container container"
-              :class="{over : item.over}"
-              @dragover   = "item.over = true"
-              @dragleave  = "item.over = false"
-              @drop       = "handleDrop(index)">
+            <drop v-else class="drop container"
+              :class="{over : this.over}"
+              @dragover   = "this.over = true"
+              @dragleave  = "this.over = false"
+              @drop       = "handleDrop">
               {{ item.message }}
               {{ item.over }}
             </drop>
@@ -54,54 +34,27 @@
 <script>
 import Vue from 'vue';
 import { Drag, Drop} from 'vue-drag-drop';
-import vueSlider from 'vue-slider-component';
-import { Draggable } from 'draggable-vue-directive'
-import anime from 'animejs'
+import chronoScene from './chronoScene.vue'
 
 export default {
   name: 'HelloWorld',
-  directives: { Draggable},
-  components: { Drag, Drop, vueSlider},
-  mounted: function () {
-    this.$nextTick(function () {
-      // Code that will run only after the
-    // entire view has been rendered
-    console.log("mounted", document.getElementById('uuu'));
-    });
-    anime({
-      targets: document.getElementById('uuu'),
-      opacity: .8, // Animate all divs opacity to .8
-      duration: 2000,
-      rotate: 180,
-      backgroundColor: '#FFF' // Animate all divs background color to #FFF
-    });
-  },
+  components: { Drag, Drop, chronoScene},
   data () {
     return {
-      imageData: "",
-      assets: [],
       items: [
         { message: 'Foo' , over : false, type: "default"}
-      ],
-      draggableValue: {
-        boundingRect: {left:30, top:30, right:200, bottom:200}
-      },
-      msg: 'Welcome to Your Vue.js App'
+      ]
     }
   },
   methods: {
-      resize(newRect) {
-        this.width  = newRect.width;
-        this.height = newRect.height;
-        this.top    = newRect.top;
-        this.left   = newRect.left;
-      },
-			handleDrop(data) {
+			handleDrop(data, transferData) {
+        console.log("data ", data, transferData);
 				this.over = false;
+
         this.items.push({
           message : 'zad',
           over    : false,
-          type    : "map_scenario",
+          type    : data.type,
           value   : {
             value: 9,
             width: 280,
@@ -124,41 +77,6 @@ export default {
            }
          });
 			},
-      handleFileDrop(data, event) {
-				event.preventDefault();
-        //console.log("event ", event);
-				const files = event.dataTransfer.files;
-
-        if (files && files[0]) {
-               // create a new FileReader to read this image and convert to base64 format
-               var reader = new FileReader();
-               // Define a callback function to run, when FileReader finishes its job
-               reader.onload = (e) => {
-                   // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
-                   // Read image as base64 and set to imageData
-                   this.imageData = e.target.result;
-               }
-               // Start the reader job - read file as a data url (base64 format)
-               reader.readAsDataURL(files[0]);
-        }else{
-          this.assets.push({layerX: event.layerX, layerY: event.layerY});
-          setTimeout(function afterTwoSeconds() {
-            console.log('2')
-            console.log("* --->", document.getElementById('zanime'));
-            anime({
-              targets: document.getElementById('zanime'),
-              opacity: .8, // Animate all divs opacity to .8
-              duration: 2000,
-              translateX: [
-                { value: 100, duration: 1200 },
-                { value: 0, duration: 800 }
-              ],
-              backgroundColor: '#FFF' // Animate all divs background color to #FFF
-            });
-          }, 100)
-
-        }
-			},
 	}
 }
 </script>
@@ -166,70 +84,9 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-html {
-  height: 100%;
-}
-
-.relative{
-  position:   relative;
-}
-
-.isfullheigth{
-  height:100%;
-  margin: 0px;
-  padding: 0px;
-}
-
-.flow-container {
-  height: 100%;
-}
-
-.map-scenario {
-  height: 250px;
-  margin-top: 20px;
-  margin: 20px;
-  padding: 0px;
-  display: flex;
-  flex-flow:column;
-  background: black;
-}
-
-.slide-epoc-range{
-  flex: 0 1 auto;
-  background: red;
-}
-
-.map-preview{
-  position:   absolute;
-  flex:       1 1 auto;
-  overflow:   hidden;
-  height:     100%;
-  z-index: 1;
-}
-
-.map-insert{
-  position: absolute;
-  flex: 1 1 auto;
-  width: 100%;
-  padding: 20px;
-  height: 100%;
-  z-index: 5;
-  background-color: blue;
-  opacity:    0.6;
-  outline-offset: -10px;
-  outline: 1px solid white;
-}
-
-.asset{
-  position: absolute;
-  width: 60px;
-  height: 60px;
-  background-color: red;
-  z-index: 10;
-}
-
 .over {
   border-color: #aaa;
   background: #ccc;
 }
+
 </style>
---
+++ src/components/chronoScene.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="map-scenario isfullheigth">
+    <vue-slider v-bind="propitem.value" class="slide-epoc-range"></vue-slider>
+
+    <div class="columns isfullheigth">
+      <div class="column has-background-primary is-one-fifth">
+        <div>elmt</div>
+        <a class="button is-primary is-inverted is-outlined">carte</a>
+      </div> <!-- column -->
+
+      <div class="column isfullheigth relative">
+        <div id="zanime" class="asset" style="position: absolute;" v-for="(asset, index) in assets" v-draggable="draggableValue">
+            {{asset.layerX}} {{asset.layerY}}
+        </div> <!-- asset-->
+        <div class="map-preview" v-if="mapBackground.length > 0">
+          <img :src="mapBackground">
+        </div> <!-- map-preview -->
+        <drop class="drop map-insert" @drop="handleFileDrop">insert map</drop>
+      </div> <!-- column -->
+    </div> <!-- columns -->
+
+  </div> <!-- if -->
+</template>
+
+<script>
+import Vue from 'vue';
+import {Drop} from 'vue-drag-drop';
+import vueSlider from 'vue-slider-component';
+import { Draggable } from 'draggable-vue-directive'
+import anime from 'animejs'
+
+export default {
+  name: 'chronoScene',
+  directives: { Draggable},
+  components: { Drop, vueSlider},
+  props: ['propitem'],
+  data () {
+    return {
+      mapBackground: "",
+      assets: [],
+      items: [
+        { message: 'Foo' , over : false, type: "default"}
+      ],
+      draggableValue: {
+        boundingRect: {left:30, top:30, right:200, bottom:200}
+      },
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+      resize(newRect) {
+        this.width  = newRect.width;
+        this.height = newRect.height;
+        this.top    = newRect.top;
+        this.left   = newRect.left;
+      },
+      handleFileDrop(data, event) {
+				event.preventDefault();
+				const files = event.dataTransfer.files;
+
+        if (files && files[0]) {
+               var reader = new FileReader();
+
+               reader.onload = (e) => {
+                   this.mapBackground = e.target.result;
+               }
+
+               reader.readAsDataURL(files[0]);
+        }else{
+          this.assets.push({layerX: event.layerX, layerY: event.layerY});
+          setTimeout(function afterTwoSeconds() {
+            anime({
+              targets: document.getElementById('zanime'),
+              opacity: .8, // Animate all divs opacity to .8
+              duration: 2000,
+              translateX: [
+                { value: 100, duration: 1200 },
+                { value: 0, duration: 800 }
+              ],
+              backgroundColor: '#FFF'
+            });
+          }, 100)
+        }
+			},
+	}
+}
+</script>
+
+<style scoped>
+
+.relative{
+  position: relative;
+}
+
+.isfullheigth{
+  height:100%;
+  margin: 0px;
+  padding: 0px;
+}
+
+.map-scenario {
+  height: 250px;
+  margin-top: 20px;
+  margin: 20px;
+  padding: 0px;
+  display: flex;
+  flex-flow:column;
+  background: black;
+}
+
+.slide-epoc-range{
+  flex: 0 1 auto;
+  background: red;
+}
+
+.map-preview{
+  position:   absolute;
+  flex:       1 1 auto;
+  overflow:   hidden;
+  height:     100%;
+  z-index: 1;
+}
+
+.map-insert{
+  position: absolute;
+  flex: 1 1 auto;
+  width: 100%;
+  padding: 20px;
+  height: 100%;
+  z-index: 5;
+  background-color: blue;
+  opacity:    0.6;
+  outline-offset: -10px;
+  outline: 1px solid white;
+}
+
+.asset{
+  position: absolute;
+  width: 60px;
+  height: 60px;
+  background-color: red;
+  z-index: 10;
+}
+</style>
